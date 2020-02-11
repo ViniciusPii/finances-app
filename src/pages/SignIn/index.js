29c734fcac8d9d87bbc03173d3f0827d6d45a13e
Pii <pii@Piis-iMac.local>
2020-02-11 20:02:38 -0300

    Inicio da estilização

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
+import {
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  SignUpLink,
+  SignUpText,
+} from './styles';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <Background>
+      <Container>
+        <Logo source={require('../../assets/logo.png')} />
+
+        <AreaInput>
+          <Input
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            placeholder="Senha"
+            autoCorrect={false}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={text => setPassword(text)}
+          />
+        </AreaInput>
+
+        <SubmitButton onPress={() => {}}>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <SignUpLink onPress={() => {}}>
+          <SignUpText>Criar conta gratuita</SignUpText>
+        </SignUpLink>
+      </Container>
+    </Background>
   );
 }
